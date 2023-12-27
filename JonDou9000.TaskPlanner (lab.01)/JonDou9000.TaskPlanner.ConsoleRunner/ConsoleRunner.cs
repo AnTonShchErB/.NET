5ddb6b0b8e837ec61afb1750271f37387db9edf1
DESKTOP-HHH8UU2\User <titan.t10099@gmail.com>
2023-12-27 13:06:41 +0200

    no message

---
+++ JonDou9000.TaskPlanner (lab.01)/JonDou9000.TaskPlanner.ConsoleRunner/ConsoleRunner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using JonDou9000.TaskPlanner.Domain.Logic;
+using JonDou9000.TaskPlanner.Domain.Models;
+using JonDou9000.TaskPlanner.Domain.Models.Enums;
+
+internal static class ConsoleRunner
+{
+    public static void Main(string[] args)
+    {
+
+        var taskPlanner = new SimpleTaskPlanner();
+
+        var workItems = new WorkItem[]
+        {
+            new WorkItem { Title = "Task 1", DueDate = DateTime.Now.AddDays(3), Priority = Priority.High },
+            new WorkItem { Title = "Task 2", DueDate = DateTime.Now.AddDays(1), Priority = Priority.Medium },
+            new WorkItem { Title = "Task 3", DueDate = DateTime.Now.AddDays(2), Priority = Priority.Urgent }
+        };
+
+        var sortedWorkItems = taskPlanner.CreatePlan(workItems);
+
+        foreach (var item in sortedWorkItems)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadLine();
+    }
+}
+
---
+++ JonDou9000.TaskPlanner (lab.01)/JonDou9000.TaskPlanner.Domain.Logic/Domain.Logic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using JonDou9000.TaskPlanner.Domain.Models;
+
+namespace JonDou9000.TaskPlanner.Domain.Logic
+{
+    public class SimpleTaskPlanner
+    {
+        public WorkItem[] CreatePlan(WorkItem[] items)
+        {
+            var itemsAsList = new List<WorkItem>(items);
+            itemsAsList.Sort(CompareWorkItems);
+            return itemsAsList.ToArray();
+        }
+
+        private static int CompareWorkItems(WorkItem firstItem, WorkItem secondItem)
+        {
+            if (firstItem.Priority != secondItem.Priority)
+                return secondItem.Priority.CompareTo(firstItem.Priority);
+
+            if (firstItem.DueDate != secondItem.DueDate)
+                return firstItem.DueDate.CompareTo(secondItem.DueDate);
+
+            return string.Compare(firstItem.Title, secondItem.Title, StringComparison.Ordinal);
+        }
+    }
+}
